4778706be670dc0ac317a82187f0df841b00b830
zuoerrimo <weiyang007@yeah.net>
2017-03-10 11:51:55 +0800

    简单的vue显示，注：vue2.0的引用方式

--- entry.js
+++ entry.js
@@ -1,3 +1,10 @@
 document.getElementById('app').innerHTML = 'first webpack code'
 require('./first.js');
 require("./style.css");
+var Vue = require('vue/dist/vue.min.js');
+new Vue({
+  el: '#container',
+  data: {
+    message: 'hello'
+  }
+})
